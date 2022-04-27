765b744785a1fa9d5fd51dc6b31d900acfc13953
thomas.le-vot <thomas.le-vot@algosecure.fr>
2022-04-27 18:48:32 +0200

    find keyword in all file type

--- CredsHarvester.py
+++ CredsHarvester.py
@@ -1,4 +1,6 @@
 import logging
+from termios import VDISCARD
+from numpy import vectorize
 import typer
 from ftplib import FTP
 from typing import Optional
@@ -12,119 +14,134 @@ import textract
 import os
 import random
 import string
+import docx2txt
 
 app = typer.Typer()
 
+
 @app.command()
 def smb(IP: Optional[str] = typer.Option(..., "-h"),
-         username: Optional[str] = typer.Option(..., "-u"),
-         password: Optional[str] = typer.Option(..., "-p"),
-         port: Optional[str] = typer.Option(139, "-P"),
-         ext_file: Optional[Path] = typer.Option(None, "-w")):
+        username: Optional[str] = typer.Option(..., "-u"),
+        password: Optional[str] = typer.Option(..., "-p"),
+        port: Optional[str] = typer.Option(139, "-P"),
+        ext_file: Optional[Path] = typer.Option(None, "-w", ),
+        keywords_file: Optional[Path] = typer.Option(None, "-k")):
 
     conn = SMBConnection(username, password, "", "")
+
     def retrieveTextSpecial(file_object):
         try:
-            # print('retrieveText', file_object.name)
-            # os.rename(file_object.name, file_object.name + ".docx")
             text = textract.process(file_object.name)
-            return text      
-        except Exception as e: 
+            print(text)
+            return text
+        except Exception as e:
             os.remove(file_object.name)
-            typer.echo("Error while parsing special file " + file_object.name + " with exception: " + str(e))
+            print("Error while parsing special file " +
+                  file_object.name + " with exception: " + str(e))
+            return "textractfailed"
 
     def filter_by_ext(share, filename, IP):
-        line_counter = 0 
+        line_counter = 0
         hits = 0
         file_obj = tempfile.NamedTemporaryFile()
         file_ext = (filename.split('/')[-1]).split('.')[-1] or "empty"
-        if not ext_file.exists():
-                typer.secho("ext_file.txt not found", fg=typer.colors.RED)
-                raise typer.Exit()
-                
         if ext_file.is_file():
             text = ext_file.read_text()
-            # print("file_ext", file_ext)
-            # if file_ext.lower() in ['docx','doc','docx','eml','epub','gif','jpg','mp3','msg','odt','ogg','pdf','png','pptx','ps','rtf','tiff','tif','wav','xlsx','xls']:
-
             if file_ext.lower() in text:
-                filesize = (conn.getAttributes(share, filename)).file_size 
-                file_attributes, filesize = conn.retrieveFile(share, filename, file_obj)
-                typer.secho("Found interesting file: " + filename, fg=typer.colors.GREEN)
-                specialfile = open(str(''.join(random.choices(string.ascii_uppercase, k = 5))) + "." +file_ext , "ab")
-                file_attributes, filesize = conn.retrieveFile(share, filename, specialfile)
-                lines = (retrieveTextSpecial(specialfile)).split(b'\n')
-                specialfile.close()
-                try:
-                    os.remove(specialfile.name)
-                except Exception as e:
-                    typer.echo("Error deleting the temp file: " + specialfile.name)
-            else:
-                file_obj.seek(0)                
-                lines = file_obj.readlines()
-                            
-            if len(lines) > 0: 
-                for line in lines:
-                    line_counter+=1 
+                # display all files with extension
+                # typer.secho("Found interesting extension: " +
+                #             filename, fg=typer.colors.WHITE)
+
+                if file_ext.lower() in ['docx', 'doc', 'zip', 'eml', 'epub', 'gif', 'jpg', 'mp3', 'msg', 'odt', 'ogg', 'pdf', 'png', 'pptx', 'ps', 'rtf', 'tiff', 'tif', 'wav', 'xlsx', 'xls']:
+                    file = filename.split('/')[2]
                     try:
-                        if 'wifi' in line.decode('UTF-8'):
-                            typer.secho("Found words in this line : " + line.decode('UTF-8'), fg=typer.colors.GREEN)
-                    except Exception as e: 
-                        typer.echo("Encountered exception while reading file: " + file_ext + " | Exception: " + str(e), err=True)
-                    # print("line", line)
-                    # try: 
-                    # #  if passwordHW((line.decode("utf-8")).strip("\n"), filename,to_match, line_counter, IP, share):
-                    # #       hits += 1
-                    # #       if hits >= options.hits:
-                    # #           logger.info("Reached max hits for " + filename
-                    # #           break  
-                    # except Exception as e: 
-                    # #    logger.warning("Encountered exception while reading file: " + file_ext + " | Exception: " + str(e))
-                    # #    if isinstance(file_obj, (io.RawIOBase, io.BufferedIOBase)):
-                    # #       options.file_extensions_black = options.file_extensions_black + "," + file_ext
-                    #    break
-            file_obj.close() 
+                        with open(file, 'wb') as fp:
+                            conn.retrieveFile(share, filename, fp)
+                            text = textract.process(fp.name)
+                            text = text.decode("utf-8").replace("\n", " ").lower()
+                            # typer.echo(text)
+                            if len(text) > 0:
+                                with open(keywords_file) as f:
+                                    # Here f is the file-like object
+                                    read_data = f.read().split('\n')
+                                if keywords_file.is_file():
+                                    for item in read_data:
+                                        match = False
+                                        if item in text:
+                                            match = True
+                                        if match:
+                                            typer.secho("Found " + item + " keywords in : " + filename, fg=typer.colors.GREEN)
+                            os.remove(fp.name)
+                    except Exception as e:
+                        typer.echo("Detected error while read file " +
+                                   fp.name + " with message " + str(e))
 
-
+                # if other file type                                   
+                else:
+                    try:
+                        with tempfile.NamedTemporaryFile() as file_obj:
+                            conn.retrieveFile(share, filename, file_obj)
+                            file_obj.seek(0)
+                            line = file_obj.read()
+                            line = line.decode(
+                                'utf-8', 'ignore').translate({ord('\u0000'): None})
+                            if len(line) > 0:
+                                with open(keywords_file) as f:
+                                    read_data = f.readlines()
+                                if keywords_file.is_file():
+                                    for item in read_data:
+                                        match = False
+                                        if item in line:
+                                            match = True
+                                        if match:
+                                            typer.secho(
+                                                "Found " + item + " keywords in : " + filename, fg=typer.colors.GREEN)
+                            file_obj.close()
+                    except Exception as e:
+                        typer.echo("Detected error while read file " + item + " with message " + str(e))
+                        
+            file_obj.close()
         elif ext_file.is_dir():
             typer.echo("Config is a directory, will use all its config files")
-        #    self.db.insertFileFinding(filename, share, IP, retrieveTimes(share,filename))
+        #    Add to BDD
 
-    def explore_path(path,shared_folder,IP):
-           #print (depth)
-           try:
-             for p in conn.listPath(shared_folder, path):
-                 if p.filename!='.' and p.filename!='..':
-                     parentPath = path
-                     if not parentPath.endswith('/'):
-                         parentPath += '/'
-                     if p.isDirectory:   
+    def explore_path(path, shared_folder, IP):
+        try:
+            for p in conn.listPath(shared_folder, path):
+                if p.filename != '.' and p.filename != '..':
+                    parentPath = path
+                    if not parentPath.endswith('/'):
+                        parentPath += '/'
+                    if p.isDirectory:
                         # print(" Goto Directory => ", parentPath+p.filename)
-                        explore_path(parentPath+p.filename,shared_folder,IP)        
-                     else:
+                        explore_path(parentPath+p.filename, shared_folder, IP)
+                    else:
                         #  print( 'File found, go parsing : '+ parentPath+p.filename)
-                         filter_by_ext(shared_folder, parentPath+p.filename, IP)
-           except Exception as e: 
-              print("Error while listing paths in shares: " + str(e))
-        
+                        filter_by_ext(shared_folder, parentPath+p.filename, IP)
+        except Exception as e:
+            print("Error while listing paths in shares: " + str(e))
+
     def connect(conn):
         try:
             conn.connect(IP, port)
             print("[+] SMB Connection Success on ", IP)
         except Exception as e:
-             logging.warning("Detected error while connecting to " + str(IP) + " with message " + str(e))
-        try:   
+            logging.warning("Detected error while connecting to " +
+                            str(IP) + " with message " + str(e))
+        try:
             shares = conn.listShares()
         except Exception as e:
-            logging.warning("Detected error while listing shares on "  + str(IP) + " with message " + str(e))
+            logging.warning("Detected error while listing shares on " +
+                            str(IP) + " with message " + str(e))
 
         for share in shares:
             if not share.isSpecial and share.name not in ['NETLOGON', 'IPC$', "ADMIN$"]:
-                print("[*] Share name:  %s " % share.name + '=> ' + share.comments)
+                print("[*] Share name:  %s " %
+                      share.name + '=> ' + share.comments)
                 print('Listing file in share: ' + share.name)
-                explore_path("/",share.name,IP)
+                explore_path("/", share.name, IP)
     connect(conn)
-   
+
 
 @app.command()
 def ssh(host: Optional[str] = typer.Option(..., "-h"),
@@ -142,9 +159,9 @@ def ssh(host: Optional[str] = typer.Option(..., "-h"),
 
 @app.command()
 def sftp(host: Optional[str] = typer.Option(..., "-h"),
-        username: Optional[str] = typer.Option(..., "-u"),
-        password: Optional[str] = typer.Option(..., "-p"),
-        port: Optional[int] = typer.Option(22, "-P")):
+         username: Optional[str] = typer.Option(..., "-u"),
+         password: Optional[str] = typer.Option(..., "-p"),
+         port: Optional[int] = typer.Option(22, "-P")):
     command = "ls"
     transport = paramiko.Transport((host, port))
     transport.connect(None, username, password)
@@ -164,14 +181,16 @@ def sftp(host: Optional[str] = typer.Option(..., "-h"),
         print("Error : download fail")
     raise typer.Exit()
 
+
 @app.command()
 def ftp(
-        host: Optional[str] = typer.Option(..., "-h", help="Ip or hostname of target."),
+        host: Optional[str] = typer.Option(...,
+                                           "-h", help="Ip or hostname of target."),
         username: Optional[str] = typer.Option(..., "-u", help="Username"),
         password: Optional[str] = typer.Option(..., "-p", help="Password"),
         port: Optional[int] = typer.Option(21, "-P")):
     try:
-        ftp = FTP(host) # connexion à l'hôte
+        ftp = FTP(host)  # connexion à l'hôte
     except:
         print("Invalid host")
         exit()
@@ -195,7 +214,6 @@ def ftp(
         print('error')
     ftp.quit()
 
+
 if __name__ == "__main__":
     app()
-
-
